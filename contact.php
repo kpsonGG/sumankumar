fca8c80aeb75f96955450ad9760b2ecdc7bcf5e1
Suman kumar <80208204+Suman-kumar23@users.noreply.github.com>
2021-07-27 13:23:56 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,15 +3,16 @@
 $name= $_REQUEST['Name'];
 $email= $_REQUEST['Email'];
 $message= $_REQUEST['Message'];
+$subject= $_REQUEST['Subject'];
 
 // check input fields
-if(empty($name) || empty($email) || empty($message)){
+if(empty($name) || empty($email) || empty($message) || empty($subject){
     echo "Please fill all the fields";
 }
 else{
     mail("sumankumar14963@gmail.com","portfolio message",$message,"from: $name <$email>");
 
-    echo  " <script type='text/javascript'>alert('your message sent succesfully');
+    echo  " <script type='text/javascript'>alert('Your message sent succesfully');
     window.history.go(-1);
     </script>";
 }
